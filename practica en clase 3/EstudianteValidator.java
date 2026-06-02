644d4002e5a198eb930b73e184707ddb5027c079
h4n1el <66563648+h4n1el@users.noreply.github.com>
2026-06-01 22:40:36 -0400

    ahora si

---
+++ practica en clase 3/EstudianteValidator.java
@@ -0,0 +1,11 @@
+public class EstudianteValidator extends Exception
+{
+    public EstudianteValidator(String mesaje)
+    {
+        super(mensaje);
+    }
+    public EstudianteValidator(String mensaje)
+    {
+        super(mensaje);
+    }
+}
